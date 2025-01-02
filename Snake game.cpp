b3ae8718ed5aeaded2e53d373198058b6e53c0e7
Isayasurusi <ijonesreuzy@gmail.com>
2025-01-02 10:40:03 +0300

    Create Snake game.cpp

---
+++ Snake game.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void run();
+void printMap();
+void initMap();
+void move(int dx, int dy);
+void update();
+void changeDirection(char key);
+void clearScreen();
+void generateFood();
+
+char getMapValue(int value);
